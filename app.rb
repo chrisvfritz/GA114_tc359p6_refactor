953d95614ed9fedd14fd676429a8152d89e84904
Antonio Gurgel <ag@antoniogurgel.com>
2015-02-09 13:15:59 -0500

    Sort curr_month holidays; add birth_month ones.

--- app.rb
+++ app.rb
@@ -8,9 +8,10 @@ class MyWebApp < Sinatra::Base
     @birth_month = Date.new(1993,1)
     @curr_month  = Date.today
     @country     = 'us'
-    @hApi_curr   = HolidApi.get
+    # Thanks for sort help, http://www.rubyinside.com/how-to/ruby-sort-hash
+    @hApi_curr   = HolidApi.get.sort_by { |date,list| date}
     @hApi_birth  = HolidApi.get(country:"#{@country}", year:"#{@birth_month.year}",
-                               month:"#{@birth_month.month}")
+                                month:"#{@birth_month.month}")
     erb :index
   end
 end
